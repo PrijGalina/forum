cbddac2009283f5b9215a763df1ddb5ba9d8fa45
Galia <g_prij@mail.ru>
2023-08-21 15:13:40 +0300

    hook useHover added

--- src/components/post-item/index.jsx
+++ src/components/post-item/index.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import { useNavigate } from 'react-router-dom';
+import { useHover } from '../../hooks/useHover';
 import { Button } from '../ui/button';
 import cl from './index.module.css';
 
 export const PostItem = ({post, remove, number}) => {
   const navigate  = useNavigate();
-  
+  const ref = useRef();
+  const isHovering = useHover(ref);
+
   return (
-    <div className={cl.post}>
+    <div className={cl.post} ref={ref} style={{background: isHovering ? '#2b3b371a' : 'transparent'}}>
       <div>
         <strong>{post.id}. {post.title}</strong>
         <p>{post.body}</p>
